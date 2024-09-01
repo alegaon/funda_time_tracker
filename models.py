0df739cd988da9aef8234581a4071b3aa86d032e
Agustin Enriquez <agustin_enriquez@hotmail.com>
2024-09-01 20:37:17 -0300

    Update models.py

--- models.py
+++ models.py
@@ -9,13 +9,15 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# create a Shift model with a foreign key to the User model
 class Shift(db.Model):
-    username = db.Column(db.String(80), db.ForeignKey('user.username'), primary_key=True)
-    date = db.Column(db.Date, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
     break_shift = db.Column(db.Enum('yes', 'no'), nullable=True, default='no')
 
+    user = db.relationship('User', backref=db.backref('shifts', lazy=True))
+
     def __repr__(self):
-        return f'<Shift {self.username} {self.date} {self.start_time}-{self.end_time}>'
+        return f'<Shift {self.user.username} {self.date} {self.start_time}-{self.end_time}>'
