00b6c763e57423f0ab66207c4f55679f9b60fefa
Agustin Enriquez <agustin_enriquez@hotmail.com>
2024-10-15 15:18:55 -0300

    agenda navbar

--- frontend/src/components/Agenda.js
+++ frontend/src/components/Agenda.js
@@ -10,6 +10,31 @@ const Agenda = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
+  const getUserData = async () => {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    
+    try {
+      const response = await axios.get(`http://localhost:5000/user/${username}/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(response.data);
+    } catch (error) {
+      console.error('Error getting user data:', error);
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      getUserData();
+      console.log(user);
+    }
+  }, [user]);
+
+
   return (
     <div className="container">
       <Navbar user={user}/>
